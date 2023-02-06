27a5211dab18f3407a1f88737dc48afd86321ee2
Derek Cortez <derekcortez16@gmail.com>
2023-02-06 02:32:13 -0600

    Initial Commit

---
+++ Proyecto/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Proyecto
+//
+//  Created by Derek Cortez Ibarra on 06/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Proyecto/ProyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoApp.swift
+//  Proyecto
+//
+//  Created by Derek Cortez Ibarra on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
